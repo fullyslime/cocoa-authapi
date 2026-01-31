9f4b6fc004684bee0d8fd10fd039713eb64efe69
bdrgn <166161143+blackdragonnnnn@users.noreply.github.com>
2026-01-31 15:37:30 +0000

    Update user API response messages and usage URL

--- api/user.js
+++ api/user.js
@@ -1,7 +1,7 @@
 // cocoa-authapi
 // user.js
 // checks the validity of a specific userid.
-// USAGE: https://domain/api/user?USERID=user_to_check
+// USAGE: https://rrowauth.vercel.app/api/user?USERID=user_to_check
 
 export default async function handler(req, res) {
   try {
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
     if (!userId) return res.status(400).send("enter the user's ID you wish to check.");
 
     const BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
-    if (!BOT_TOKEN) return res.status(500).send("server isn' configured.");
+    if (!BOT_TOKEN) return res.status(500).send("server isn't configured.");
 
     const GUILD_ID = "1463615235674869772";
     const ROLE_ID = "1463628337418338616";
@@ -19,13 +19,13 @@ export default async function handler(req, res) {
       { headers: { Authorization: `Bot ${BOT_TOKEN}` } }
     );
 
-    if (!tokenRes.ok) return res.status(403).send("cocoa access denied");
+    if (!tokenRes.ok) return res.status(403).send("rrow access denied");
 
     const member = await memberRes.json();
     const hasRole = member.roles.includes(ROLE_ID);
 
-    if (hasRole) return res.status(200).send("cocoa access granted");
-    else return return res.status(403).send("cocoa access denied");
+    if (hasRole) return res.status(200).send("rrow access granted");
+    else return return res.status(403).send("rrow access denied");
 
   } catch (err) {
     console.error("API CRASH:", err);
